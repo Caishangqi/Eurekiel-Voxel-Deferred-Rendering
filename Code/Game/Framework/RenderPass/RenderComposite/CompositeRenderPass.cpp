c1743a68e84c81e1cee7abb7dabdf6dc84b490d6
CaIzii <2470159730@qq.com>
2026-01-15 02:24:22 -0600

    Implement composite render pass with fullscreen quad and depth transitions
    Signed-off-by: CaIzii <2470159730@qq.com>

--- Code/Game/Framework/RenderPass/RenderComposite/CompositeRenderPass.cpp
+++ Code/Game/Framework/RenderPass/RenderComposite/CompositeRenderPass.cpp
@@ -1,7 +1,39 @@
 ﻿#include "CompositeRenderPass.hpp"
 
+#include "Engine/Core/VertexUtils.hpp"
+#include "Engine/Graphic/Helper/VertexConversionHelper.hpp"
+#include "Engine/Graphic/Integration/RendererSubsystem.hpp"
+#include "Engine/Graphic/Resource/VertexLayout/Layouts/Vertex_PCUTBNLayout.hpp"
+#include "Engine/Graphic/Resource/Buffer/D12VertexBuffer.hpp"
+#include "Engine/Graphic/Shader/Common/ShaderCompilationHelper.hpp"
+#include "Engine/Graphic/Shader/Uniform/MatricesUniforms.hpp"
+#include "Engine/Graphic/Shader/Uniform/UniformManager.hpp"
+#include "Engine/Graphic/Target/DepthTextureProvider.hpp"
+
 CompositeRenderPass::CompositeRenderPass()
 {
+    enigma::graphic::ShaderCompileOptions shaderCompileOptions;
+    shaderCompileOptions.enableDebugInfo = true;
+
+    m_shaderProgram = g_theRendererSubsystem->CreateShaderProgramFromFiles(
+        ".enigma/assets/engine/shaders/program/composite.vs.hlsl",
+        ".enigma/assets/engine/shaders/program/composite.ps.hlsl",
+        "composite",
+        shaderCompileOptions
+    );
+
+    /// Fullquads vertex buffer
+    std::vector<Vertex_PCU> verts;
+    AABB2                   fullQuadsAABB2;
+    auto                    renderHeight = g_theRendererSubsystem->GetConfiguration().renderHeight;
+    auto                    renderWidth  = g_theRendererSubsystem->GetConfiguration().renderWidth;
+    fullQuadsAABB2.SetDimensions(Vec2((float)renderWidth, (float)renderHeight));
+    AddVertsForAABB2D(verts, fullQuadsAABB2, Rgba8::WHITE);
+    auto vertsTBN           = VertexConversionHelper::ToPCUTBNVector(verts);
+    m_fullQuadsVertexBuffer = D3D12RenderSystem::CreateVertexBuffer(vertsTBN.size() * sizeof(Vertex_PCUTBN), sizeof(Vertex_PCUTBN), vertsTBN.data());
+
+    /// Ortho Camera
+    m_compositeCamera = std::make_unique<OrthographicCamera>(Vec3::ZERO, EulerAngles(), fullQuadsAABB2.m_mins, fullQuadsAABB2.m_maxs, -1.0f, 1.0f);
 }
 
 CompositeRenderPass::~CompositeRenderPass()
@@ -10,12 +42,52 @@ CompositeRenderPass::~CompositeRenderPass()
 
 void CompositeRenderPass::Execute()
 {
+    BeginPass();
+    // Logic
+    MatricesUniforms matricesUniform;
+    m_compositeCamera->UpdateMatrixUniforms(matricesUniform);
+    g_theRendererSubsystem->GetUniformManager()->UploadBuffer(matricesUniform);
+    g_theRendererSubsystem->DrawVertexBuffer(m_fullQuadsVertexBuffer);
+
+    EndPass();
 }
 
 void CompositeRenderPass::BeginPass()
 {
+    // [FIX] Transition depthtex0 to PIXEL_SHADER_RESOURCE for sampling
+    // D3D12 Rule: Cannot read (SRV) and write (DSV) same resource simultaneously
+    // [REFACTOR] Use D12DepthTexture::TransitionToShaderResource() per OCP
+    auto* depthProvider = static_cast<enigma::graphic::DepthTextureProvider*>(g_theRendererSubsystem->GetProvider(RTType::DepthTex));
+    if (depthProvider)
+    {
+        auto depthTex = depthProvider->GetDepthTexture(0);
+        if (depthTex)
+        {
+            depthTex->TransitionToShaderResource();
+        }
+    }
+    g_theRendererSubsystem->SetVertexLayout(Vertex_PCUTBNLayout::Get());
+    g_theRendererSubsystem->SetDepthMode(DepthMode::Enabled);
+    g_theRendererSubsystem->SetRasterizationConfig(RasterizationConfig::NoCull());
+    if (m_shaderProgram)
+    {
+        g_theRendererSubsystem->UseProgram(m_shaderProgram, {{RTType::ColorTex, 0}});
+    }
 }
 
 void CompositeRenderPass::EndPass()
 {
+    g_theRendererSubsystem->SetRasterizationConfig(RasterizationConfig::CullBack());
+
+    // [FIX] Transition depthtex0 back to DEPTH_WRITE for subsequent passes
+    // [REFACTOR] Use D12DepthTexture::TransitionToDepthWrite() per OCP
+    auto* depthProvider = static_cast<enigma::graphic::DepthTextureProvider*>(g_theRendererSubsystem->GetProvider(RTType::DepthTex));
+    if (depthProvider)
+    {
+        auto depthTex = depthProvider->GetDepthTexture(0);
+        if (depthTex)
+        {
+            depthTex->TransitionToDepthWrite();
+        }
+    }
 }
--- Code/Game/Framework/RenderPass/RenderComposite/CompositeRenderPass.hpp
+++ Code/Game/Framework/RenderPass/RenderComposite/CompositeRenderPass.hpp
@@ -1,5 +1,14 @@
 ﻿#pragma once
+#include <memory>
+
+#include "Engine/Graphic/Camera/OrthographicCamera.hpp"
 #include "Game/Framework/RenderPass/SceneRenderPass.hpp"
+
+namespace enigma::graphic
+{
+    class D12VertexBuffer;
+    class ShaderProgram;
+}
 
 class CompositeRenderPass : public SceneRenderPass
 {
@@ -11,4 +20,10 @@ public:
 protected:
     void BeginPass() override;
     void EndPass() override;
+
+private:
+    std::shared_ptr<enigma::graphic::ShaderProgram>   m_shaderProgram         = nullptr;
+    std::shared_ptr<enigma::graphic::D12VertexBuffer> m_fullQuadsVertexBuffer = nullptr;
+
+    std::unique_ptr<enigma::graphic::OrthographicCamera> m_compositeCamera = nullptr;
 };
